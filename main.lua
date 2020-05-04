20a64b9f228b357a761c36a6e009b0a9736a3c91
HellowThar <49899462+HellowThar@users.noreply.github.com>
2020-05-03 16:21:01 -1000

    alpha testing ready

--- main.lua
+++ main.lua
@@ -3,9 +3,13 @@ local HBD = HereBeDragons
 --helper functions
 local function getDistance()
     local player_x, player_y, player_instance = HBD:GetPlayerWorldPosition()
-    local tar_x, tar_y, tar_instance = HBD:GetUnitWorldPosition("party1")
-    local distance, delta_x, delta_y = HBD:GetWorldDistance(player_instance, player_x, player_y, tar_x, tar_y)
-    return distance
+    local target_x, target_y, target_instance = HBD:GetUnitWorldPosition("party1")
+    if player_instance == target_instance then
+        local distance, delta_x, delta_y = HBD:GetWorldDistance(player_instance, player_x, player_y, target_x, target_y)
+        return distance
+    else
+        return -1
+    end
 end
 
 local function turnUIOff()
@@ -33,44 +37,13 @@ end
 --main frame
 C_ChatInfo.RegisterAddonMessagePrefix("hideandseek")
 
-local HideAndSeekFrame = CreateFrame("Button", "HideAndSeekFrame", UIParent, "UIPanelButtonTemplate")
-local HASF = HideAndSeekFrame
-HASF:SetSize(100,33)
+local HASF = CreateFrame("FRAME", "HideAndSeekFrame", UIParent)
+
+HASF:SetSize(400,100)
 HASF:SetPoint("CENTER")
 HASF:SetMovable(true)
 
-HASF:SetText("Hint")
-HASF:SetScript("OnClick", function()
-    local distance = getDistance()
-    if distance < 5 then
-        print("You found them!")
-        C_ChatInfo.SendAddonMessage("hideandseek", "you've been found", "PARTY")
-        turnUIOn()
-        HASF:SetText("Hide!")
-        HASF:Disable()
-    elseif distance < 40 then
-        HASF:SetText("HOT")
-    elseif distance < 100 then
-        HASF:SetText("WARM")
-    else
-        HASF:SetText("COLD")
-    end
-end)
-
-HASF:RegisterEvent("CHAT_MSG_ADDON")
-
-HASF:SetScript("OnEvent", function(self, event, ...)
-    local prefix, text, channel, sender, target, zoneChannelID, localId, name, instanceID = ...
-
-    if text == "you've been found" then
-        turnUIOff()
-        HASF:SetText("Hint")
-        HASF:Enable()
-    end
-end)
-
-
-local function HasHandler(arg)
+local function HasfHandler(arg)
     if arg == "help" then
         print("type '/has enable' to turn the game on or '/has disable' to turn the game off")
     elseif arg == "enable" then
@@ -83,6 +56,67 @@ end
 SLASH_HAS1 = '/hideandseek'
 SLASH_HAS2 = '/has'
 
-SlashCmdList["HAS"] = HasHandler
+SlashCmdList["HAS"] = HasfHandler
+
+local StartButton = CreateFrame("Button", "StartButton", HASF, "UIPanelButtonTemplate")
+StartButton:SetSize(100,33)
+StartButton:SetPoint("LEFT", HASF, "LEFT")
+StartButton:SetText("Start")
+StartButton:SetScript("OnClick", function(self, event, ...)
+    local distance = getDistance()
+    HASF.OtherPlayer = UnitName("party1")
+
+    if distance < 0 then
+        print("Must be in same zone to start the game!")
+    else
+        local success = C_ChatInfo.SendAddonMessage("hideandseek", "partner started game", "WHISPER", HASF.OtherPlayer)
+        if success then
+            HintButton:Enable()
+            self:Hide()
+        end
+    end
+
+end)
+
+local HintButton = CreateFrame("Button", "HintButton", HASF, "UIPanelButtonTemplate")
+HintButton:SetSize(100,33)
+HintButton:SetPoint("CENTER", HASF, "CENTER")
+HintButton:SetText("Hint")
+HintButton:Disable()
+HintButton:SetScript("OnClick", function()
+    local distance = getDistance()
+    
+    if distance < 0 then
+        HintButton:SetText("You are in the wrong zone!")
+    elseif distance < 5 then
+        print("You found them!")
+        C_ChatInfo.SendAddonMessage("hideandseek", "you've been found", "WHISPER", HASF.OtherPlayer)
+        turnUIOn()
+        HintButton:SetText("Hide!")
+        HintButton:Disable()
+    elseif distance < 40 then
+        HintButton:SetText("HOT")
+    elseif distance < 100 then
+        HintButton:SetText("WARM")
+    else
+        HintButton:SetText("COLD")
+    end
+end)
+
+HASF:RegisterEvent("CHAT_MSG_ADDON")
+
+-- event handler
+
+HASF:SetScript("OnEvent", function(self, event, ...)
+    local prefix, text, channel, sender, target, zoneChannelID, localId, name, instanceID = ...
+    
+    if text == "you've been found" then
+        turnUIOff()
+        HintButton:SetText("Hint")
+        HintButton:Enable()
+    elseif text == "partner started game" then
+        HintButton:SetText("Hide!")
+    end
+end)
 
 print("Hide and Seek loaded! Type '/has help' for help")
